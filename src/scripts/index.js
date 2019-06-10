d1b3483ab627c4c223eb7c1f745a5a4db3bbd8ba
Tetris <tetriscurry@gmail.com>
2019-06-10 08:10:59 +0100

    Class test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,24 @@
-import '../styles/index.scss';
+let app = (function(){
+    let getId = function(...v){ return `${v} ${this.carId}`; };
+    return { getId };
+})();
 
-console.log('webpack starterkit');
+class Vehicle {
+    constructor(name, wheels){
+        this.name = name;
+        this.wheels = wheels;
+    }
+    getWheelsPerSide(){ return this.wheels;}
+}
+class Car extends Vehicle {
+    constructor(name, wheels, size){
+        super(name, wheels);
+        this.size = size;
+    }
+    getWheelsPerSide(){ return this.wheels/4;}
+}
+
+Car.prototype.getNameLength = function(){ return this.name.length; };
+let car = new Car('Type',4,'Big');
+let carB = new Car();
+console.log(car, car.name, car.getNameLength()   );
